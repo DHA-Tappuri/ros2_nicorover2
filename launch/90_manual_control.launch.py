545c832471bd8e8142620af0cf31641333b13760
Your Name <you@example.com>
2025-02-20 22:54:59 +0900

    working commit

--- launch/90_manual_control.launch.py
+++ launch/90_manual_control.launch.py
@@ -20,6 +20,7 @@ def generate_launch_description():
         PythonLaunchDescriptionSource(other_launch_file),
         launch_arguments={'some_arg': 'value'}.items()
     )
+    ld.add_action(nodes_delta2g)
 
     # MID 360 3D lidar    
     other_launch_file = os.path.join( get_package_share_directory('livox_ros_driver2'), 'launch_ROS2', 'msg_MID360_launch.py' )
@@ -27,6 +28,14 @@ def generate_launch_description():
         PythonLaunchDescriptionSource(other_launch_file),
         launch_arguments={'some_arg': 'value'}.items()
     )
+    ld.add_action(nodes_mid360)
+    node_pcd2 = Node(
+        package    = 'livox_to_pointcloud2',
+        executable = 'livox_to_pointcloud2_node',
+        name       = 'livox_to_pointcloud2',
+        remappings = [('/livox_pointcloud', '/livox/lidar'), ('/converted_pointcloud2', '/livox/pcd2')]
+    )
+    ld.add_action(node_pcd2)    
 
     # DDSM controller
     other_launch_file = os.path.join( get_package_share_directory('ros2_ddsm_robot'), 'launch', '90_manual_control.launch.py' )
@@ -34,6 +43,7 @@ def generate_launch_description():
         PythonLaunchDescriptionSource(other_launch_file),
         launch_arguments={'some_arg': 'value'}.items()
     )
+    ld.add_action(nodes_ddsm)
 
     # rviz    
     config_file = PathJoinSubstitution([ FindPackageShare('ros2_nicorover2'), 'config', 'nicorover2.rviz' ])
@@ -44,10 +54,6 @@ def generate_launch_description():
         namespace  = 'rviz2',
         arguments  = ['-d', config_file]
     )
-        
-    ld.add_action(nodes_delta2g)
-    ld.add_action(nodes_mid360)
-    ld.add_action(nodes_ddsm)
     ld.add_action(node_rviz)
 
     return ld
